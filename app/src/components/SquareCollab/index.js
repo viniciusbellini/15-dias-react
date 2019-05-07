24810b2e3215ceb430a5d00b30694a81b805df15
Vinicius Bellini <viniciusbellini@gmail.com>
2019-05-06 22:57:58 -0300

    Final correção desafio dia 02

--- app/src/components/SquareCollab/index.js
+++ app/src/components/SquareCollab/index.js
@@ -3,21 +3,9 @@ import React, {Component} from "react";
 import "./styles.css";
 
 class SquareCollab extends Component{
-    constructor(props){
-        super(props);
-        this.state = {
-            content: "X"
-        }
-    }
-    handleClick = () => {
-        const newState = {content: "X"};
-
-        if (this.state.content === "X"){
-            newState.content = "O";
-        }
-
-        this.setState(newState);
-    }
+    state = {content: "X"};
+    
+    handleClick = () => this.setState(this.state.content === "X" ? {content: "O"} : {content: "X"})
 
     render(){
         return(
